41cffaaaffaa6b2e55c55ce5bf216f16374fc5ec
marcompp <marcompp@al.insper.edu.br>
2022-03-21 22:35:12 -0300

    entrega

--- main.py
+++ main.py
@@ -5,29 +5,7 @@ from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, Field, HttpUrl, EmailStr
 
-class UnicornException(Exception):
-    def __init__(self, name: str):
-        self.name = name
-
-class CommonQueryParams:
-    def __init__(self, q: Optional[str] = None, skip: int = 0, limit: int = 100):
-        self.q = q
-        self.skip = skip
-        self.limit = limit
-
-
-class Tags(Enum):
-    items = "items"
-    users = "users"
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
+app = FastAPI()
 
 class Item(BaseModel):
     name: str = Field(..., example="Foo")
@@ -38,33 +16,6 @@ class Item(BaseModel):
     tax: Optional[float] = Field(None, example=3.2)
     tags: List[str] = []
     images: Optional[List[Image]] = None
-
-class BaseItem(BaseModel):
-    description: str
-    type: str
-
-
-class CarItem(BaseItem):
-    type = "car"
-
-
-class PlaneItem(BaseItem):
-    type = "plane"
-    size: int
-
-
-    
-class User(BaseModel):
-    username: str
-    full_name: Optional[str] = None
-
-
-
-class Offer(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    items: List[Item]
 
 class UserBase(BaseModel):
     username: str
@@ -83,170 +34,37 @@ class UserOut(UserBase):
 class UserInDB(UserBase):
     hashed_password: str
 
-def query_extractor(q: Optional[str] = None):
-    return q
-
-def query_or_cookie_extractor(
-    q: str = Depends(query_extractor), last_query: Optional[str] = Cookie(None)
-):
-    if not q:
-        return last_query
-    return q
-
-def fake_password_hasher(raw_password: str):
-    return "supersecret" + raw_password
-
-
-def fake_save_user(user_in: UserIn):
-    hashed_password = fake_password_hasher(user_in.password)
-    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
-    print("User saved! ..not really")
-    return user_in_db
-
-async def verify_token(x_token: str = Header(...)):
-    if x_token != "fake-super-secret-token":
-        raise HTTPException(status_code=400, detail="X-Token header invalid")
-
-
-async def verify_key(x_key: str = Header(...)):
-    if x_key != "fake-super-secret-key":
-        raise HTTPException(status_code=400, detail="X-Key header invalid")
-    return x_key
-
-#app = FastAPI()
-app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
-
-
-
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 items = {"foo": "The Foo Wrestlers"}
 
-async def common_parameters(q: Optional[str] = None, skip: int = 0, limit: int = 100):
-    return {"q": q, "skip": skip, "limit": limit}
-
-
-@app.exception_handler(UnicornException)
-async def unicorn_exception_handler(request: Request, exc: UnicornException):
-    return JSONResponse(
-        status_code=418,
-        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
-    )
-
-#post
-@app.post("/login/")
-async def login(username: str = Form(...), password: str = Form(...)):
-    return {"username": username}
-
-@app.post("/items/", 
-    status_code=status.HTTP_201_CREATED,
-    response_model=Item,
-    summary="Create an item",
-    response_description="The created item",
-)
-async def create_item(item: Item):
-    """
-    Create an item with all the information:
-
-    - **name**: each item must have a name
-    - **description**: a long description
-    - **price**: required
-    - **tax**: if the item doesn't have tax, you can omit this
-    - **tags**: a set of unique tag strings for this item
-    """
-
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
-
-@app.post("/user/", response_model=UserOut)
-async def create_user(user_in: UserIn):
-    user_saved = fake_save_user(user_in)
-    return user_saved
-
-@app.post("/images/multiple/")
-async def create_multiple_images(images: List[Image]):
-    return images
-
-@app.post("/index-weights/")
-async def create_index_weights(weights: Dict[int, float]):
-    return weights
-
-@app.post("/files/")
-async def create_file(
-    file: bytes = File(...), fileb: UploadFile = File(...), token: str = Form(...)
-):
-    return {
-        "file_size": len(file),
-        "token": token,
-        "fileb_content_type": fileb.content_type,
-    }
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(
-    file: UploadFile = File(..., description="A file read as UploadFile")
-):
-    return {"filename": file.filename}
-
-
-#put
 @app.put("/items/{item_id}", response_model=Item)
 async def update_item(item_id: str, item: Item):
     update_item_encoded = jsonable_encoder(item)
     items[item_id] = update_item_encoded
     return update_item_encoded
 
-#patch
-@app.patch("/items/{item_id}", response_model=Item)
-async def update_item(item_id: str, item: Item):
-    stored_item_data = items[item_id]
-    stored_item_model = Item(**stored_item_data)
-    update_data = item.dict(exclude_unset=True)
-    updated_item = stored_item_model.copy(update=update_data)
-    items[item_id] = jsonable_encoder(updated_item)
-    return updated_item
-
-#get
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-@app.get("/items/")
-async def read_query(query_or_default: str = Depends(query_or_cookie_extractor)):
-    return {"q_or_cookie": query_or_default}
-
-
-@app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
+@app.get("/items/{item_id}")
 async def read_item(item_id: str):
     return items[item_id]
 
-@app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
-async def read_items():
-    return [{"item": "Foo"}, {"item": "Bar"}]
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    if (item_id not in items):
+        raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
+    return {"item_id": item_id}
 
 
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
+@app.delete("/items/{item_id}")
+async def read_item(item_id: str):
+    if (item in items):
+        del items[item]
 
 
-@app.get("/users/{user_id}")
+@app.post("/user/", response_model=UserOut)
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
+
+@app.get("/users/{user_id}, response_model=UserOut")
 async def read_user(user_id: str):
     return {"user_id": user_id}
 
@@ -262,13 +80,3 @@ async def read_user_item(
             {"description": "This is an amazing item that has a long description"}
         )
     return item
-
-@app.get("/keyword-weights/", response_model=Dict[str, float])
-async def read_keyword_weights():
-    return {"foo": 2.3, "bar": 3.4}
-
-@app.get("/unicorns/{name}")
-async def read_unicorn(name: str):
-    if name == "yolo":
-        raise UnicornException(name=name)
-    return {"unicorn_name": name}
